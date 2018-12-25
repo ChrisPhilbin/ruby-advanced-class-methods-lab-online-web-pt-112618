af0611a7ef2cf26879bdebb8803cbbec40f87f84
Chris Philbin <chris@chrisphilbin.net>
2018-12-25 03:51:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,10 @@ class Song
     end
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
   def self.destroy_all
     self.all.clear
   end
